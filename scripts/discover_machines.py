fe290996457c054e29f1a6538bb36ae20a771aa7
ralph <yawen@microstrategy.com>
2014-10-13 22:59:07 +0800

    add research for machine discovery

--- scripts/discover_machines.py
+++ scripts/discover_machines.py
@@ -2,4 +2,12 @@ __author__ = 'yawen'
 '''
 machines.csv
 machine, add time
+
+use python nmap lib
+
+http://serverfault.com/questions/63233/best-method-and-tools-for-local-ip-scanning/63242#63242
+
+https://www.google.com/search?q=python+nmap
+http://xael.org/norman/python/python-nmap/
+https://pypi.python.org/pypi/python-libnmap/0.6.1
 '''
