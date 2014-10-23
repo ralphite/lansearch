c383ee71a8e6067e3dc3cf3363c77b12b03eb737
Ralph Wen <ralph.wen@gmail.com>
2014-10-23 17:44:05 +0800

    add check_machine_availability

--- scripts/discover_machines.py
+++ scripts/discover_machines.py
@@ -8,11 +8,31 @@ import os
 import sys
 import nmap
 import socket
+import ping
 import win32net
 import win32com.client
 from smb.SMBConnection import SMBConnection
 
 from config import config
+
+
+class DummyStream:
+    """ DummyStream behaves like a stream but does nothing. """
+
+    def __init__(self):
+        pass
+
+    def write(self, data):
+        pass
+
+    def read(self, data):
+        pass
+
+    def flush(self):
+        pass
+
+    def close(self):
+        pass
 
 
 def get_share_list(user, password, localhost_name, remote_target_name):
@@ -30,16 +50,46 @@ def get_machines_in_domain(domain):
     nt = adsi.GetObject("", "WinNT:")
     result = nt.OpenDSObject("WinNT://%s" % domain, "", "", 0)
     result.Filter = ["computer"]
-    for machine in result:
-        yield machine.Name
+    for computer in result:
+        yield computer.Name
+
+
+def get_belonging_domain():
+    domain_controller = win32net.NetGetDCName(None, None)
+    return win32net.NetUserModalsGet(domain_controller, 2)['domain_name']
+
+
+def check_machine_availability(machine_name):
+    try:
+        # Copy old print deals
+        old_printerators = [sys.stdout, sys.stderr, sys.stdin,
+                            sys.__stdout__, sys.__stderr__, sys.__stdin__][:]
+        # redirect all print deals
+        sys.stdout = DummyStream()
+        sys.stderr = DummyStream()
+        sys.stdin = DummyStream()
+        sys.__stdout__ = DummyStream()
+        sys.__stderr__ = DummyStream()
+        sys.__stdin__ = DummyStream()
+
+        res = ping.quiet_ping(machine_name, 5, 5)
+
+        # Turn printing back on!
+        sys.stdout, sys.stderr, sys.stdin, sys.__stdout__, \
+        sys.__stderr__, sys.__stdin__ = old_printerators
+
+        if res[1]:
+            return True
+        else:
+            return False
+    except socket.error, e:
+        pass
 
 
 if __name__ == '__main__':
     print get_share_list(config['user'], config['password'],
                          config['localhost'], 'chn-yawen')
 
-    domain_controller = win32net.NetGetDCName(None, None)
-    domain_name = win32net.NetUserModalsGet(domain_controller, 2)['domain_name']
-    print "Listing machines in", domain_name
-    for machine in get_machines_in_domain(domain_name):
-        print machine
+    for machine in get_machines_in_domain('corp'):
+        if machine[:4] == 'CHN-' and check_machine_availability(machine):
+            print machine
